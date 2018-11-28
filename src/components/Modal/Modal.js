259b70078a9d2f29fdd1c83e570f5dc2242f6b6c
dmitry.marokhonov <dmitry.marokhonov@gmail.com>
2018-11-28 15:27:13 +0200

    Animations via CSSTransition

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,32 +1,28 @@
 import React from "react";
-import Transition from "react-transition-group/Transition";
+import CSSTransition from "react-transition-group/CSSTransition";
 
 import classes from "./Modal.module.scss";
 
 const animationTiming = {
   enter: 400,
   exit: 1000
-}
+};
 
 const modal = props => {
   return (
-    <Transition mountOnEnter unmountOnExit in={props.show} timeout={animationTiming}>
-      {state => {
-        let cssClasses = [classes.Modal];
-        state === "entering"
-          ? cssClasses.push(classes.ModalOpen)
-          : state === "exiting" && cssClasses.push(classes.ModalClosed);
-
-        return (
-          <div className={cssClasses.join(" ")}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>
-              Dismiss
-            </button>
-          </div>
-        );
-      }}
-    </Transition>
+    <CSSTransition mountOnEnter unmountOnExit in={props.show} timeout={animationTiming} classNames={{
+      enter: "",
+      enterActive: classes.ModalOpen,
+      exit: "",
+      exitActive: classes.ModalClosed
+    }}>
+      <div className={classes.Modal}>
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 
